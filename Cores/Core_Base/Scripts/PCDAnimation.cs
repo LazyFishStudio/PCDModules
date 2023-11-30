abdd4edb9bd5cecf49322c08b4ffb30fbe22473e
samooz <1339004604@qq.com>
2023-11-30 09:36:52 +0800

    新功能上传-PCDAnimation上传

---
+++ Cores/Core_Base/Scripts/PCDAnimation.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCDAnimation : ScriptableObject {
+    public string poseLayerName;    // 动画图层名称
+    public string faceLayerName;    // 表情图层名称
+    public string archoringPointLayerName;   // 锚点图层名称
+}
