31949006a185298e8de58c1471abb72693d8d5e2
samooz <1339004604@qq.com>
2024-01-21 13:31:04 +0800

    给 PCDFollow 添加 LateUpdate 选项

--- PCDAnim/MoveAndRotation/PCDFollow.cs
+++ PCDAnim/MoveAndRotation/PCDFollow.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode]
 public class PCDFollow : MonoBehaviour {
+    public bool isLateUpdate;
     public Transform followTarget;
     public Vector3 followOffset;
 
     void Update() {
+        if (isLateUpdate) {
+            return;
+        }
+        transform.position = followTarget.position + followOffset;
+    }
+
+    void LateUpdate() {
+        if (!isLateUpdate) {
+            return;
+        }
         transform.position = followTarget.position + followOffset;
     }
 }
